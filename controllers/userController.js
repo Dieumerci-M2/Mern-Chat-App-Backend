909abf3a8bbd0686453833fdbbbb22741a55912c
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-01-19 15:24:05 +0100

    feat : add validation congifuration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,25 @@
 import bcrypt from 'bcrypt';
 import User from '../Model/UserModel.js';
 import generateToken from '../config/generateToken.js';
+import { signupValidator, signinValidator } from '../validator/validator.js';
 
-const registerUser = async (req, res) => {
-  const {
-    nom, email, password, picture,
-  } = req.body;
+const registerUser = async ( req, res, next ) => {
+  
+   const { nom, email, password, picture } = req.body;
+  
+  try {
 
-  if (!nom || !email || !password) {
-    return res.status(400).send({ message: 'Please complete all the field' });
-  }
+    const result = await signupValidator.validateAsync({...req.body})
 
-  const existUser = await User.findOne({ email });
+    const existUser = await User.findOne({ email });
 
   if (existUser) {
     res.status(400);
     throw new Error('This user are already exist');
   }
+    if ( result ) {
 
-  const user = await User.create({
+       const user = await User.create({
     nom,
     email,
     password,
@@ -33,38 +34,51 @@ const registerUser = async (req, res) => {
       picture: user.picture,
       token: generateToken(user._id),
     });
-  } else {
+  } 
+    
+  }
+  else {
     res.status(404);
     throw new Error('user not found');
   }
+    
+  } catch (error) {
+    next(error)
+  }
+  
+
 };
 
 const authUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({
+    const result = signinValidator.validateAsync( { ...req.body } )
+    if ( result ) {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({
         message: "pas d'utilisateur a cet email",
         statut: false,
       });
+      }
+      const isOk = await bcrypt.compare(password, user.password);
+        if (!isOk) {
+          return res.json({
+          message: 'mot de passe incorrect',
+          statut: false,
+        });
+      }
+      if (user && isOk) {
+        res.json({
+          _id: user._id,
+          nom: user.nom,
+          email: user.email,
+          picture: user.picture,
+          token: generateToken(user._id),
+        });
+      }
     }
-    const isOk = await bcrypt.compare(password, user.password);
-    if (!isOk) {
-      return res.json({
-        message: 'mot de passe incorrect',
-        statut: false,
-      });
-    }
-    if (user && isOk) {
-      res.json({
-        _id: user._id,
-        nom: user.nom,
-        email: user.email,
-        picture: user.picture,
-        token: generateToken(user._id),
-      });
-    }
+    
   } catch (ex) {
     next(ex);
   }
